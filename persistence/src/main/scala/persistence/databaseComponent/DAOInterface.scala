ef1ca588d5b9c60fd32f00873078f37ac650ada9
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-04-28 23:33:10 +0200

    wip: removed explicit init

--- persistence/src/main/scala/persistence/databaseComponent/DAOInterface.scala
+++ persistence/src/main/scala/persistence/databaseComponent/DAOInterface.scala
@@ -7,7 +7,6 @@ import scala.util.Try
 case class GameTableData(state: String, boardSize: String, playerSize: String, playerType: String, currPlayerIndex: Int, playerData: Vector[(Int, Int)])
 
 trait DAOInterface:
-  def init: Unit
   def create: Try[Int]
   def read: Try[GameTableData]
   def update(gameTableData: GameTableData): Try[Int]
