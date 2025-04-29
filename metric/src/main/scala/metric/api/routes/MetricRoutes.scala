814b9f49146cf356b23683ae2e3662d5c3259fc9
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-04-29 14:23:38 +0200

    wip: using GameStats for type safety

--- metric/src/main/scala/metric/api/routes/MetricRoutes.scala
+++ metric/src/main/scala/metric/api/routes/MetricRoutes.scala
@@ -4,12 +4,19 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import de.github.dotsandboxes.lib.{GameStats, PlayerStats}
+import io.circe.generic.auto._
+import io.circe.parser.decode
+import io.circe.syntax._
 import metric.api.module.MetricModule.given_DAOInterface as daoInterface
-import play.api.libs.json.{JsValue, Json}
-import scala.util.{Failure, Success}
 import org.slf4j.LoggerFactory
+import play.api.libs.json.{JsValue, Json, OFormat}
+import scala.util.{Failure, Success}
 
 class MetricRoutes:
+  implicit val playerStatsFormat: OFormat[PlayerStats] = Json.format[PlayerStats]
+  implicit val gameStatsFormat: OFormat[GameStats] = Json.format[GameStats]
+
   private val logger = LoggerFactory.getLogger(getClass.getName.init)
 
   def metricRoutes: Route = handleExceptions(exceptionHandler) {
@@ -50,21 +57,22 @@ class MetricRoutes:
         val jsonValue: JsValue = Json.parse(json)
         val playerNames = (jsonValue \ "playerNames").as[Seq[String]].toVector
 
-        val stats = Json.obj(
+        val statsJson = Json.obj(
           "totalDuration" -> daoInterface.getTotalGameDuration,
           "playerStats" -> Json.obj(
             playerNames.map { name =>
               name -> Json.obj(
-                "avgMoveDuration"    -> daoInterface.getAvgMoveDuration(name),
-                "minMoveDuration"    -> daoInterface.getMinMoveDuration(name),
-                "maxMoveDuration"    -> daoInterface.getMaxMoveDuration(name),
-                "longestMoveStreak"  -> daoInterface.getLongestMoveStreak(name),
-                "numOfTotalMoves"    -> daoInterface.getNumOfTotalMoves(name)
+                "avgMoveDuration" -> daoInterface.getAvgMoveDuration(name),
+                "minMoveDuration" -> daoInterface.getMinMoveDuration(name),
+                "maxMoveDuration" -> daoInterface.getMaxMoveDuration(name),
+                "longestMoveStreak" -> daoInterface.getLongestMoveStreak(name),
+                "numOfTotalMoves" -> daoInterface.getNumOfTotalMoves(name)
               )
             }*
           )
         )
-        complete(stats.toString)
+        val gameStats: GameStats = statsJson.as[GameStats]
+        complete(gameStats.asJson.toString)
       }
     }
   }
