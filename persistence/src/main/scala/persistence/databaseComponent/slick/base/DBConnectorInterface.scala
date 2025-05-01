8169491bf2afac5743751023bbe92db3c7ca3436
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-05-01 22:25:24 +0200

    wip: added system coordinated shutdown with db disconnect

--- persistence/src/main/scala/persistence/databaseComponent/slick/base/DBConnectorInterface.scala
+++ persistence/src/main/scala/persistence/databaseComponent/slick/base/DBConnectorInterface.scala
@@ -2,8 +2,10 @@ package persistence.databaseComponent.slick.base
 
 import slick.dbio.{DBIOAction, Effect, NoStream}
 import slick.jdbc.JdbcBackend.JdbcDatabaseDef
+import scala.concurrent.Future
+import akka.Done
 
 trait DBConnectorInterface:
   val db: JdbcDatabaseDef
   def connect(setup: DBIOAction[Unit, NoStream, Effect.Schema]): Unit
-  def disconnect: Unit
+  def disconnect: Future[Done]
--- persistence/src/main/scala/persistence/databaseComponent/slick/base/connectors/H2Connector.scala
+++ persistence/src/main/scala/persistence/databaseComponent/slick/base/connectors/H2Connector.scala
@@ -1,9 +1,11 @@
 package persistence.databaseComponent.slick.base.connectors
 
+import akka.Done
+import akka.actor.{ActorSystem, CoordinatedShutdown}
 import common.config.DatabaseConfig._
 import org.slf4j.LoggerFactory
 import persistence.databaseComponent.slick.base.DBConnectorInterface
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
@@ -11,7 +13,13 @@ import slick.dbio.{DBIOAction, Effect, NoStream}
 import slick.jdbc.JdbcBackend.{Database, JdbcDatabaseDef}
 
 class H2Connector extends DBConnectorInterface:
+  private implicit val system: ActorSystem = ActorSystem(getClass.getSimpleName.init)
+
   private val logger = LoggerFactory.getLogger(getClass.getName.init)
+
+  CoordinatedShutdown(system).addTask(CoordinatedShutdown.PhaseServiceStop, "shutdown-postgres-connection") { () =>
+    disconnect
+  }
 
   override val db = Database.forURL(
     url = PERSISTENCE_DB_H2_URL,
@@ -31,6 +39,7 @@ class H2Connector extends DBConnectorInterface:
         retry(retries - 1, setup)(database)
       case Failure(exception) => logger.error(s"Persistence Service [Database] -- Could not establish a connection to the H2 database: ${exception.getMessage}")
 
-  override def disconnect: Unit =
+  override def disconnect: Future[Done] =
     logger.info("Persistence Service [Database] -- Closing H2 database connection...")
     db.close
+    Future.successful(Done)
