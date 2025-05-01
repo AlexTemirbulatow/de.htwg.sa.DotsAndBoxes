99f43ac94b13fa1c0d4b3844e4cf6a7f7ba69145
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-05-01 23:46:41 +0200

    chore: refactoring

--- persistence/src/main/scala/persistence/api/module/PersistenceModule.scala
+++ persistence/src/main/scala/persistence/api/module/PersistenceModule.scala
@@ -1,8 +1,8 @@
 package persistence.api.module
 
 import persistence.databaseComponent.DAOInterface
-import persistence.databaseComponent.slick.dao.Slick
 import persistence.databaseComponent.slick.connector
+import persistence.databaseComponent.slick.dao.Slick
 
 object PersistenceModule:
   given DAOInterface = Slick(new connector.PostgresConnector)
