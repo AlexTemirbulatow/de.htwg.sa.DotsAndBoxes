ba5b99ad6f40b5487a8fe70962dc625b42c4d41f
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-05-03 13:46:35 +0200

    wip: implemented MongoDB CRUD

--- persistence/src/main/scala/persistence/databaseComponent/mongoDB/dao/MongoDAO.scala
+++ persistence/src/main/scala/persistence/databaseComponent/mongoDB/dao/MongoDAO.scala
@@ -2,16 +2,20 @@ package persistence.databaseComponent.mongoDB.dao
 
 import common.config.DatabaseConfig.PERSISTENCE_DB_MONGO_COLLECTION_NAME
 import de.github.dotsandboxes.lib._
-import org.mongodb.scala.Document
+import org.bson.BsonValue
 import org.mongodb.scala._
-import org.mongodb.scala.bson.Document
+import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.model.ReplaceOptions
+import org.mongodb.scala.model.Updates._
 import org.slf4j.LoggerFactory
 import persistence.databaseComponent.mongoDB.base.DBConnectorInterface
 import persistence.databaseComponent.{DAOInterface, GameTableData}
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success, Try}
+import org.bson.BsonArray
+import org.mongodb.scala.bson.BsonDocument
 
 object Mongo:
   def apply(dbConnector: DBConnectorInterface): DAOInterface = new MongoDAO(dbConnector)
@@ -28,7 +32,7 @@ object Mongo:
       "playerSize"          -> PlayerSize.Two.toString,
       "playerType"          -> PlayerType.Human.toString,
       "currentPlayerIndex"  -> 0,
-      "playerData"          -> list.zipWithIndex.map { case (player, index) => Document("index" -> index, "points" -> player.points, "active" -> false) }
+      "playerData"          -> list.zipWithIndex.map { case (player, index) => playerDocument(index, player.points) }
     )
 
     dbConnector.connect
@@ -39,6 +43,18 @@ object Mongo:
       case Success(_)         => logger.info(s"Persistence Service [Database] -- Initial collection successfully created")
       case Failure(exception) => logger.error(s"Persistence Service [Database] -- Could not create initial collection: ${exception.getMessage}")
 
+    private def playerDocument(playerData: (Int, Int)): BsonDocument =
+      BsonDocument("index" -> playerData._1, "points" -> playerData._2)
+
+    private def playerData(playerDataValue: BsonValue): Vector[(Int, Int)] =
+      playerDataValue.asArray.getValues.asScala.toVector
+        .map(_.asDocument())
+        .map { doc =>
+          val index = doc.getInt32("index").getValue
+          val points = doc.getInt32("points").getValue
+          (index, points)
+        }
+
     override def create: Try[Int] = Try {
       Await.result(gameCollection.replaceOne(
         filter = Document("_id" -> MONGO_COLLECTION_GAME_ID),
@@ -48,8 +64,46 @@ object Mongo:
       MONGO_COLLECTION_GAME_ID
     }
 
-    override def read: Try[GameTableData] = ???
+    override def read: Try[GameTableData] = Try {
+      val document: Document = Await.result(
+        gameCollection.find(equal("_id", MONGO_COLLECTION_GAME_ID)).first().toFuture,
+        5.seconds
+      )
+      GameTableData(
+        state = document.getString("state"),
+        boardSize = document.getString("boardSize"),
+        playerSize = document.getString("playerSize"),
+        playerType = document.getString("playerType"),
+        currPlayerIndex = document.getInteger("currentPlayerIndex"),
+        playerData = playerData(document.get("playerData").get),
+      )
+    }
 
-    override def update(gameTableData: GameTableData): Try[Int] = ???
+    override def update(gameTableData: GameTableData): Try[Int] = Try {
+      val update = combine(
+        set("state", gameTableData.state),
+        set("boardSize", gameTableData.boardSize),
+        set("playerSize", gameTableData.playerSize),
+        set("playerType", gameTableData.playerType),
+        set("currentPlayerIndex", gameTableData.currPlayerIndex),
+        set("playerData", new BsonArray(gameTableData.playerData.map {
+          case (index, points) => playerDocument(index, points)
+        }.asJava))
+      )
+      Await.result(
+        gameCollection.updateOne(
+          filter = equal("_id", MONGO_COLLECTION_GAME_ID),
+          update = update
+        ).toFuture,
+        5.seconds
+      )
+      MONGO_COLLECTION_GAME_ID
+    }
 
-    override def delete: Try[Int] = ???
+    override def delete: Try[Int] = Try {
+      Await.result(
+        gameCollection.deleteOne(equal("_id", MONGO_COLLECTION_GAME_ID)).toFuture,
+        5.seconds
+      )
+      MONGO_COLLECTION_GAME_ID
+    }
