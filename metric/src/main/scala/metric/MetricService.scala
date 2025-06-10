e3ea9dc9e44cdf8d7768146c88f7b6f3c7d00893
AlexTemirbulatow <temirbulatow1@gmail.com>
2025-06-11 00:34:48 +0200

    chore: refactoring

--- metric/src/main/scala/metric/MetricService.scala
+++ metric/src/main/scala/metric/MetricService.scala
@@ -4,4 +4,6 @@ import scala.concurrent.duration.Duration
 object MetricService:
   @main def startMetricsServer: Unit =
     metric.api.server.MetricHttpServer.run
+    //metric.kafka.consumer.KafkaConsumer.run
+    //metric.kafka.service.MetricKafkaService.start
     Await.result(Future.never, Duration.Inf)
